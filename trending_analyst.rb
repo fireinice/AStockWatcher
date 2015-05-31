1fb26d9d547cd638abb70cc4769021ba9c6422ad
zigler zhang <zigler.zhang@gmail.com>
2015-06-01 00:26:24 +0800

    rewrite trending analyst

--- trending_analyst.rb
+++ trending_analyst.rb
@@ -16,14 +16,59 @@ def sort_by_points(infos)
     end
   end
   s_lines.sort!{ |x,y| y.score.points <=> x.score.points }
-  ret_info = {}
+  ret_infos = {}
   s_lines.each do |l|
-    next if ret_info.has_key?(l)
-    ret_info[line_infos[l]] = infos[line_infos[l]]
+    next if ret_infos.has_key?(l)
+    ret_infos[line_infos[l]] = infos[line_infos[l]]
   end
-  ret_info
+  ret_infos
 end
 
+def formatted_print(stocks, infos)
+  infos.each do |ref, value|
+    support_lines = value[0]
+    pressure_lines = value[1]
+    stock = stocks[ref]
+    puts "=========="
+    puts "#{stock.name}, PE:#{stock.pe}, PB:#{stock.pb}"
+    # puts StockPlate.get_status(stock)
+    CalcTrendingHelper.print_info(stock, support_lines[:score])
+    CalcTrendingHelper.print_info(stock, support_lines[:points])
+    for i in (0..support_lines[:candis].size()) do
+      s_line = support_lines[:candis][i]
+      p_line = pressure_lines[i]
+      #p_line could be nil if pressure line too close to support line
+      next if p_line.nil?
+      CalcTrendingHelper.print_info(stock, s_line, p_line)
+    end
+  end
+end
+
+def filter_by_deal_diff(stocks, infos, accept_ratio)
+  accept_range =Range.new(0, accept_ratio)
+  ret_infos = {}
+  infos.each do |ref, value|
+
+    support_lines = value[0]
+    pressure_lines = value[1]
+    stock = stocks[ref]
+    next if stock.deal.nil? # skip stop trading stock
+    s_lines = []
+    p_lines = []
+    support_lines[:candis].each.with_index do |line, i|
+      # skip if line above price now more than 5% or below than 5%
+      diff = (stock.deal-line.get_point(-1)).abs
+      next if not accept_range.cover?(diff/stock.deal)
+      s_lines << line
+      p_lines << pressure_lines[i]
+    end
+    next if s_lines.empty?
+    ret_infos[ref] = infos[ref]
+    ret_infos[ref][0][:candis] = s_lines
+    ret_infos[ref][1] = p_lines
+  end
+  ret_infos
+end
 
 if $0 == __FILE__
   infos = YAML.load(File.open("trending_scan.yml"))
@@ -36,36 +81,10 @@ if $0 == __FILE__
 
   stock_infos = QQTradingDay.get_status_batch(stocks.values)
 
+  stocks.each_value { |stock| stock.update_day_trading_info!(stock_infos[stock.ref_value])}
+
+  infos = filter_by_deal_diff(stocks, infos, 0.05)
   infos = sort_by_points(infos)
 
-  infos.each do |ref, value|
-    support_lines = value[0]
-    pressure_lines = value[1]
-    stock = stocks[ref]
-    stock.update_day_trading_info!(stock_infos[stock.ref_value])
-    next if stock.deal.nil? # skip stop trading stock
-    # skip if line above price now more than 5% or below than 5%
-    accept_range =Range.new(stock.deal * 0.95, stock.deal * 1.05)
-    s_lines = []
-    p_lines = []
-    support_lines[:candis].each.with_index do |line, i|
-      next if not accept_range.cover?(line.get_point(-1))
-      s_lines << line
-      p_lines << pressure_lines[i]
-    end
-    next if s_lines.empty?
-    puts "=========="
-    puts "#{stock.name}, PE:#{stock.pe}, PB:#{stock.pb}"
-    # puts StockPlate.get_status(stock)
-    CalcTrendingHelper.print_info(stock, support_lines[:score])
-    CalcTrendingHelper.print_info(stock, support_lines[:points])
-    for i in (0..s_lines.size()) do
-      s_line = s_lines[i]
-      p_line = p_lines[i]
-      #p_line could be nil if pressure line too close to support line
-      next if p_line.nil?
-      CalcTrendingHelper.print_info(stock, s_line, p_line)
-    end
-  end
-
+  formatted_print(stocks, infos)
 end
